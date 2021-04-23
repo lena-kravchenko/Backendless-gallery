1c77cac5f934db640760b5316d857d7a888ced63
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-04-24 02:50:20 +0300

    Theme toggler added

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
+const selectElement = document.querySelector('.theme-toggler');
 
+function initialState(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+}
+/* 
+function toggleTheme() {
+  if (localStorage.getItem('theme') === 'dark-theme') {
+    initialState('dark-theme');
+  }
+
+  if (localStorage.getItem('theme') === 'backendless-theme') {
+    initialState('backendless-theme');
+  }
+
+  if (localStorage.getItem('theme') === 'light-theme') {
+    initialState('light-theme');
+  }
+} */
+
+selectElement.addEventListener('change', (event) => {
+  event.preventDefault();
+  initialState(event.target.value);
+});
